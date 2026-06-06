31dff62f013cb88c3b6f78fb903f229545ae73f6
Veeranki-guru <9885850326guru@gmail.com>
2026-06-06 11:01:46 +0530

    this message

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.rajesh86s.online
+MONGODB_HOST=172.31.47.83
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
